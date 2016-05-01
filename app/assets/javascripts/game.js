eb349e6a1caa9c7506fa7ee297f29d6cc7475043
prerakgupta <prerak@browserstack.com>
2016-05-02 03:31:08 +0530

    adding to git

---
+++ app/assets/javascripts/game.js
@@ -0,0 +1,112 @@
+$(document).ready(function(){
+  
+  var num_of_dealer_cards = 0;
+
+  $("#rules_button").on("click", function(e){
+    e.preventDefault();
+    $("#welcome").hide();
+    $("#rules").show();
+  });
+
+  $("#close_rules").on("click", function(e){
+    e.preventDefault();
+    $("#welcome").show();
+    $("#rules").hide();   
+  });
+
+  $("#proceed").on("click", function(e){
+    e.preventDefault();
+    $("#welcome").hide();
+    $("#place_bet").show();
+  });
+
+  $("#bet").on("click", function(e){
+    e.preventDefault();
+    var input = $("#amount").val();
+    if( ((input - 0) == input && (''+input).trim().length > 0) && input!=0 ){
+      $("#place_bet").hide();
+      $("#amount_playing").html("Playing for " + input + "$");
+      $("#game").show();
+
+      $("#message").html("<p style='color: green'>Dealing the first round of cards</p>");
+      delayedClick("hit", 500);
+      delayedClick("hit", 2600);
+      delayedClick("stand", 5000);
+      setTimeout(function(){
+        $("#message").html("<p style='color: green'>Hit or Stand?</p>");
+      }, 7000);
+    }
+    else
+      alert("Valid numbers only here!");
+  });
+
+  $("#next_game").on('click', function(){
+    window.location.reload(true);
+  })
+
+  $(".get_card").on("click", function(e){
+    var type = $(this).data("type");
+    var id = type + "_cards";
+    e.preventDefault();
+    if(type=="dealer"){
+      num_of_dealer_cards = num_of_dealer_cards + 1;
+    }
+    $.ajax({
+      url: '/game/get_next_card/',
+      type: 'GET',
+      contentType: 'application/json',
+      dataType: 'json',
+      data: {type: type},
+      success: function(response){
+        if(response["status"]=="ok"){
+          var div = document.getElementById(id);
+          div.innerHTML = div.innerHTML + "&nbsp &nbsp" + response["selected"];
+          $.ajax({
+            url: '/game/check_results/',
+            type: 'GET',
+            contentType: 'application/json',
+            dataType: 'json',
+            data: {type: type},
+            success: function(response){
+              console.log(response);
+              setTimeout(function(){
+                if(response["game_over"]){
+                  declareResults(response["winner"]);
+                }
+                else if(type=="dealer" && num_of_dealer_cards > 1){
+                  $("#message").hide();
+                  $("#stand").click();
+                }
+              }, 1000);
+            }
+          });
+        }
+      }
+    });
+  });
+
+  function declareResults(winner){
+    $("#message").hide();
+    var text = "";
+    if(winner=="draw"){
+      text = "Well its a draw!"
+    }
+    else if(winner=="player"){
+      text = "Congratulations you won!"
+    }
+    else if(winner=="dealer"){
+      text = "Sorry you loose. Better luck next time."
+    }
+    text = text + "<br/>"
+    var div = document.getElementById('winner_info');
+    div.innerHTML = text + div.innerHTML;
+    $(".get_card").hide();
+    $("#game_finished").show();
+  }
+
+  function delayedClick(id, duration){
+    setTimeout(function(){
+      $("#"+id).click();
+    }, duration); 
+  }
+});
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blackjack::Application.config.secret_key_base = '28b10373f53580836a99d87d1bd66279b084747ee99dc9857b5f3a96204b6da3557fb02fe74aae15546a6bfa7f4dc47a6ac34d4ebb8ee23d542d0f263738f1a3'
